4840d5640f8e7d74de57b901880f40aebc8bcba7
Gwenaël Boisnard <boisnard.gwenael@gmail.com>
2022-04-12 13:51:37 +0200

    Added springinitializr project

---
+++ src/main/java/fr/cesi/goodfood/GoodfoodApplication.java
@@ -0,0 +1,13 @@
+package fr.cesi.goodfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/cesi/goodfood/GoodfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.cesi.goodfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
