28603ed2d55b7100aa94d65099142639775343ea
Gwenaël Boisnard <boisnard.gwenael@gmail.com>
2022-06-23 20:12:04 +0200

    Added test for auth service

--- src/test/java/fr/cesi/goodfood/service/AuthServiceTest.java
+++ src/test/java/fr/cesi/goodfood/service/AuthServiceTest.java
@@ -1,8 +1,14 @@
 package fr.cesi.goodfood.service;
 
+import fr.cesi.goodfood.api.exception.UserAlreadyExistingException;
+import fr.cesi.goodfood.dto.CustomerDto;
+import fr.cesi.goodfood.dto.RestaurantDto;
 import fr.cesi.goodfood.entity.Customer;
+import fr.cesi.goodfood.entity.Restaurant;
 import fr.cesi.goodfood.payload.request.LoginRequest;
+import fr.cesi.goodfood.payload.request.RegisterCustomerRequest;
 import fr.cesi.goodfood.payload.response.JwtResponse;
+import fr.cesi.goodfood.payload.response.RegisterResponse;
 import fr.cesi.goodfood.repository.CustomerRepository;
 import fr.cesi.goodfood.security.jwt.JwtUtils;
 import org.junit.jupiter.api.BeforeAll;
@@ -19,6 +25,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import java.time.LocalDateTime;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
@@ -45,6 +53,8 @@ public class AuthServiceTest {
 
     private static Customer customer;
 
+    private static Restaurant restaurant;
+
 
     @BeforeEach
     void setUp() {
@@ -52,17 +62,52 @@ public class AuthServiceTest {
         customer = new Customer(null, "test", "test", "test@test.fr", "test", "0102030405", "test street", "test city",
                                 "test zipcode", "test region", "test country", null, LocalDateTime.now(),
                                 LocalDateTime.now(), "ROLE_CUSTOMER");
+        restaurant = new Restaurant();
         authentication.setAuthenticated(true);
     }
 
     @Test
-    void should_Return_Jwt_Token() {
+    void should_Return_Jwt_Token_For_Customer_Role() {
         LoginRequest loginRequest = new LoginRequest("test", "test");
         when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).thenReturn(
                 authentication);
         when(jwtUtils.generateToken(any(Authentication.class))).thenReturn("test token");
+        when(authentication.getPrincipal()).thenReturn(customer);
         JwtResponse jwtResponse = authService.login(loginRequest);
-        assertEquals(jwtResponse.getToken(), "test token");
+        assertEquals("test token", jwtResponse.getToken());
+        assertTrue(jwtResponse.getUserData() instanceof CustomerDto);
     }
+
+    @Test
+    void should_Return_Jwt_Token_For_Restaurant_Role() {
+        LoginRequest loginRequest = new LoginRequest("test", "test");
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).thenReturn(
+                authentication);
+        when(jwtUtils.generateToken(any(Authentication.class))).thenReturn("test token");
+        when(authentication.getPrincipal()).thenReturn(restaurant);
+        JwtResponse jwtResponse = authService.login(loginRequest);
+        assertEquals("test token", jwtResponse.getToken());
+        assertTrue(jwtResponse.getUserData() instanceof RestaurantDto);
+    }
+
+    @Test
+    void should_Return_Success_On_Register() {
+        RegisterCustomerRequest registerCustomerRequest = new RegisterCustomerRequest("test", "test", "test", "test",
+                                                                                      "test", "test", "test", "test",
+                                                                                      "test", "test");
+        when(customerRepository.save(any(Customer.class))).thenReturn(customer);
+        RegisterResponse registerResponse = authService.registerCustomer(registerCustomerRequest);
+        assertEquals(registerResponse.getStatus(), "SUCCESS");
+    }
+
+    @Test
+    void should_Throw_UserAlreadyExistingException() {
+        RegisterCustomerRequest registerCustomerRequest = new RegisterCustomerRequest("test", "test", "test", "test",
+                                                                                      "test", "test", "test", "test",
+                                                                                      "test", "test");
+        when(customerRepository.existsCustomerByEmail(anyString())).thenThrow(UserAlreadyExistingException.class);
+        assertThrows(UserAlreadyExistingException.class, () -> authService.registerCustomer(registerCustomerRequest));
+    }
+
 
 }
